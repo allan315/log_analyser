e1a01c7f1e6877cdfeedcb4af465075179899fbd
allan315 <allanpilvet@gmail.com>
2018-04-02 21:57:48 +0300

    comments

--- loganalyser.py
+++ loganalyser.py
@@ -2,19 +2,23 @@ import psycopg2
 import argparse
 import sys
 
-""" 1. most popular article
-create view view_count as
-select replace(path, '/article/', '') as slug, count(*)
-from log group by slug;"""
+# This script will query database for some statistics (described below)
+# and prints the results to standard output. It should be used from command line.
+# Currently script has 3 queries which can be accessed through arguments e.g.
+# python loganalyser.py --authors
+
+
+# Top articles query (needs to have "view_count" view). It connects articles and logs
+# using modified path in log table which is connected to slug to get view count.
 
 top_articles = """select articles.title, view_count.count from
 articles inner join view_count on articles.slug = view_count.slug
 order by view_count.count desc limit 3;"""
 
-""" 2. most popular author
-create view view_count as
-select replace(path, '/article/', '') as slug, count(*)
-from log group by slug;"""
+# Top authors query (needs to have "view_count" view). This query can be divided to
+# two steps 1. we connect view_count with articles, 2. we connect this result
+# with authors table to get author name and view count
+
 
 top_authors = """select authors.name, t.count from
 (select articles.author, articles.slug, view_count.count from articles
@@ -22,12 +26,8 @@ inner join view_count on articles.slug = view_count.slug)
 as t inner join authors on authors.id = t.author
 order by t.count desc limit 5;"""
 
-""" 3. log errors
-create view ok as select time::timestamp::date as date, status, count(*)
-from log where status like '200%' group by date, status;
-
-create view errors as select time::timestamp::date as date, status, count(*)
-from log where status like '404%' group by date, status;"""
+# Log errors query (needs to have "ok" and "errors" view). This query uses two views and
+# calculates error rate (percentage). Error rate higher than 1 will be shown.
 
 top_errors = """select to_char(date, 'Month DD, YYYY') as formated_date,
 to_char(err, '9.9%') as error_rate
@@ -36,9 +36,14 @@ from ok inner join errors on ok.date = errors.date)
 as t where err > 1 order by err desc limit 5;
 """
 
+
+# I'm using argparse to define and take arguments from command line
+
 parser = argparse.ArgumentParser(add_help=True,
                                  description='***YOU NEED TO USE ONE OF THE OPTIONAL ARGUMENTS***',  # NOQA
                                  usage='%(prog)s options: [-h] [--errors] [--authors] [--articles]')  # NOQA
+
+# add_mutually_exclusive_group() will define that only one argument is taken
 
 group = parser.add_mutually_exclusive_group()
 
@@ -50,6 +55,7 @@ group.add_argument('--articles', help='show which show top three articles',
                    action='store_true')
 args = parser.parse_args()
 
+# Connect to DB, execute queries and return data
 
 def get_from_db(query):
     db = psycopg2.connect(dbname="news")
@@ -60,8 +66,14 @@ def get_from_db(query):
     return data
 
 if __name__ == '__main__':
+
+    # Check for no arguments and print error if any
+    
     if not (args.errors or args.authors or args.articles):
         parser.error('***YOU NEED TO USE ONE OF THE OPTIONAL ARGUMENTS***')
+
+    # Check what argument was given with "if" statements and execute the appropriate SQL query
+
     if args.errors:
         data = get_from_db(top_errors)
         sys.stdout.write("Date - Time\n")
