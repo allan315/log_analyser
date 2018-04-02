66a068f76cb2d9292c298c60ed3bb7f4dfef657c
allan315 <allanpilvet@gmail.com>
2018-04-02 22:10:32 +0300

    pep8 fix

--- loganalyser.py
+++ loganalyser.py
@@ -3,21 +3,24 @@ import argparse
 import sys
 
 # This script will query database for some statistics (described below)
-# and prints the results to standard output. It should be used from command line.
+# and prints the results to standard output.
+# It should be used from command line.
 # Currently script has 3 queries which can be accessed through arguments e.g.
 # python loganalyser.py --authors
 
 
-# Top articles query (needs to have "view_count" view). It connects articles and logs
-# using modified path in log table which is connected to slug to get view count.
+# Top articles query (needs to have "view_count" view).
+# It connects articles and logs using modified path in log table which is
+# connected to slug to get view count.
 
 top_articles = """select articles.title, view_count.count from
 articles inner join view_count on articles.slug = view_count.slug
 order by view_count.count desc limit 3;"""
 
-# Top authors query (needs to have "view_count" view). This query can be divided to
-# two steps 1. we connect view_count with articles, 2. we connect this result
-# with authors table to get author name and view count
+# Top authors query (needs to have "view_count" view).
+# This query can be divided to two steps 1. we connect view_count
+# with articles, 2. we connect this result with authors table to get
+# author name and view count.
 
 
 top_authors = """select authors.name, t.count from
@@ -26,8 +29,9 @@ inner join view_count on articles.slug = view_count.slug)
 as t inner join authors on authors.id = t.author
 order by t.count desc limit 5;"""
 
-# Log errors query (needs to have "ok" and "errors" view). This query uses two views and
-# calculates error rate (percentage). Error rate higher than 1 will be shown.
+# Log errors query (needs to have "ok" and "errors" view).
+# This query uses two views and calculates error rate (percentage).
+# Error rate higher than 1 will be shown.
 
 top_errors = """select to_char(date, 'Month DD, YYYY') as formated_date,
 to_char(err, '9.9%') as error_rate
@@ -57,6 +61,7 @@ args = parser.parse_args()
 
 # Connect to DB, execute queries and return data
 
+
 def get_from_db(query):
     db = psycopg2.connect(dbname="news")
     c = db.cursor()
@@ -68,11 +73,11 @@ def get_from_db(query):
 if __name__ == '__main__':
 
     # Check for no arguments and print error if any
-    
     if not (args.errors or args.authors or args.articles):
         parser.error('***YOU NEED TO USE ONE OF THE OPTIONAL ARGUMENTS***')
 
-    # Check what argument was given with "if" statements and execute the appropriate SQL query
+    # Check what argument was given with "if" statements.
+    # Execute the appropriate SQL query.
 
     if args.errors:
         data = get_from_db(top_errors)
